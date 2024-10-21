7e07973e3b60cc1daaba9c0d2639f593f78f7fa8
GrOnK48 <vadusbeska@gmail.com>
2024-10-21 13:37:35 +0500

    Add files via upload

---
+++ akhl1/akhl1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+namespace akhl1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_one(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            stack1.Visibility = stack1.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            stack2.Visibility = stack2.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          
+            if (int.TryParse(plotnost1.Text, out int plotnost) &&
+                int.TryParse(visota1.Text, out int visota) &&
+                int.TryParse(radius1.Text, out int radius))
+            {
+             
+                bool check21 = check2.IsChecked == true;
+                bool check11 = check2.IsChecked == true;
+
+                if (check11 || check21 == true)
+                {
+                    double volume = (1.0 / 3) * Math.PI * (radius * radius) * visota;
+                    double mass = volume * plotnost;
+                    text1.Text = $"V = {volume:F2} см³, M = {mass:F2} г";
+                    stack2.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, выберите хотя бы одну опцию.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+    }
+}
