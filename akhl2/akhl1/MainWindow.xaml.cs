cb119fa1ae5087314a972b42ce9cb37963499573
GrOnK48 <vadusbeska@gmail.com>
2024-10-21 13:51:09 +0500

    Add files via upload

---
+++ akhl2/akhl1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows;
+
+namespace akhl1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_one(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            stack1.Visibility = stack1.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            stack2.Visibility = stack2.Visibility == Visibility.Visible ? Visibility.Hidden : Visibility.Visible;
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(plotnost1.Text, out int plotnost) &&
+                int.TryParse(visota1.Text, out int visota) &&
+                int.TryParse(radius1.Text, out int radius))
+            {
+                bool check11 = check1.IsChecked == true;
+                double volume = radius + visota + plotnost;
+                bool check21 = check2.IsChecked == true; 
+                double mass = radius / visota; 
+
+                string result = "SUMM = " + volume;
+
+                if (check21)
+                {
+                    result += $", НАИМЕНЬШЕЕ КРАТНОЕ = {mass}";
+                }
+
+                text1.Text = result;
+                stack2.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения.");
+            }
+        }
+
+    }
+}
+
